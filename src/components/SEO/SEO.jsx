e5bc0be193301cdd845c67b30295e3a31ca82f12
PtrwTg <mawngai0001@gmail.com>
2025-07-15 23:21:05 +0700

    ปรับปรุงข้อมูล SEO ใน SEO.jsx โดยเพิ่มข้อมูลเกี่ยวกับพิกัดภูมิศาสตร์, อีเมล, ลิงก์เว็บไซต์, และเวลาทำการ รวมถึงเปลี่ยนลิงก์รูปภาพเป็น URL ใหม่

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -1,14 +1,15 @@
 import { Helmet } from "react-helmet";
 
 const SEO = ({ title, description, structuredData }) => {
-  const logoUrl = window.location.origin + '/assets/images/LogoThaiNest.svg'; 
+  //const logoUrl = window.location.origin + '/assets/images/LogoThaiNest.svg'; 
 
   const defaultStructuredData = {
     "@context": "https://schema.org",
     "@type": "Spa",
     "name": "ThaiNest Massage & Facial",
-    "alternateName": "ThaiNest Caloundra, ThaiNest Massage, ThaiNest Facial, Thainest, Thai massage, Thai facial, Thainest Massage and Facial",
-    "image": logoUrl,
+    "alternateName": "ThaiNest Caloundra, ThaiNest Massage, ThaiNest Facial, Thainest",
+    "description": "ThaiNest Massage & Facial in Caloundra, QLD. Authentic Thai massage and facial treatments for relaxation and wellness.",
+    "image": "https://thainestmassage.com.au/assets/images/LogoThaiNest.svg",
     "address": {
       "@type": "PostalAddress",
       "streetAddress": "4/74 Bulcock Street",
@@ -17,8 +18,51 @@ const SEO = ({ title, description, structuredData }) => {
       "postalCode": "4551",
       "addressCountry": "AU"
     },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": -26.80488,
+      "longitude": 153.13115
+    },
     "telephone": "048-3063-980",
-    "url": window.location.origin
+    "email": "thainestmassage@gmail.com",
+    "url": "https://thainestmassage.com.au",
+    "priceRange": "$$",
+    "openingHoursSpecification": [
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday"
+        ],
+        "opens": "09:00",
+        "closes": "17:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Sunday",
+        "opens": "09:00",
+        "closes": "13:00"
+      }
+    ],
+    "hasMap": "https://maps.app.goo.gl/KSRNqYugvas2MiKD6",
+    "sameAs": [
+      "https://www.facebook.com/thainestmassage",
+      "https://maps.app.goo.gl/KSRNqYugvas2MiKD6"
+    ],
+    "areaServed": [
+      {
+        "@type": "Place",
+        "name": "Caloundra"
+      },
+      {
+        "@type": "Place",
+        "name": "Sunshine Coast"
+      }
+    ]
   };
 
   return (
